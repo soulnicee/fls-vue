adb1d1347b606730c96967374f8fd028817ae936
soulnicee <soulnicee@gmail.com>
2023-11-08 08:33:44 +0200

    changes

---
+++ homework-7/task-2/src/data/lists.js
@@ -0,0 +1,58 @@
+export const productList = [
+  {
+    item: 'Хліб',
+    price: 24,
+    id: 1
+  },
+  {
+    item: 'Молоко',
+    price: 31,
+    id: 2
+  },
+  {
+    item: 'Хліб',
+    price: 24,
+    id: 3
+  },
+  {
+    item: 'Ковбаса',
+    price: 350,
+    id: 4
+  },
+  {
+    item: 'Шовдарь',
+    price: 950,
+    id: 5
+  },
+  {
+    item: 'Пікниця',
+    price: 210,
+    id: 6
+  },
+  {
+    item: 'Вода',
+    price: 15,
+    id: 7
+  },
+  {
+    item: 'Ноутбук',
+    price: 52999,
+    id: 8
+  },
+  {
+    item: 'Монітор',
+    price: 10000,
+    id: 9
+  },
+]
+
+export const currencyList = [
+  {
+    id: 1,
+    name: 'Гривня'
+  },
+  {
+    id: 2,
+    name: 'Долар'
+  },
+]
