7df359f537bce44ec87790fbc59cd152cbb9a101
soulnicee <soulnicee@gmail.com>
2023-11-08 08:57:42 +0200

    preparing components

---
+++ homework-7/task-2/src/components/StoreManager.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="manager">
+    <h1 class="manager__title"></h1>
+    <div class="manager__lists">
+      <div class="manager__products products">
+        <h2 class="products__title"></h2>
+        <div class="products__list">
+          <products-list />
+        </div>
+      </div>
+      <div class="manager__cart cart">
+        <h2 class="cart__title"></h2>
+        <div class="cart__list">
+          <cart-list />
+        </div>
+      </div>
+    </div>
+    <div class="manager__currency currency">
+      <div class="currency__select">
+        <currency-changer />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ProductsList from '@/components/ProductsList.vue';
+import CartList from '@/components/CartList.vue';
+import CurrencyChanger from '@/components/CurrencyChanger.vue';
+
+export default {
+  name: 'StoreManager',
+  components: {
+    ProductsList,
+    CartList,
+    CurrencyChanger
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
--- homework-7/task-2/src/store/index.js
+++ homework-7/task-2/src/store/index.js
@@ -1,10 +1,33 @@
 import { createStore } from "vuex";
+import { productList, currencyList } from "@/data/lists.js";
 
  const store = createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
+  state() {
+    return {
+      productList: [],
+      currencyList: []
+    }
+  },
+  getters: {
+    productList:({productList}) => productList,
+    currencyList: ({currencyList}) => currencyList
+  },
+  mutations: {
+    setProductList(state, list) {
+      state.productList = list
+    },
+    setCurrencyList(state, list) {
+      state.currencyList = list
+    },
+  },
+  actions: {
+    loadProductsList({commit}) {
+      commit('setProductList', productList)
+    },
+    loadCurrencyList({commit}) {
+      commit('setCurrencyList', currencyList)
+    }
+  },
   modules: {},
 });
 export default store
