22f3e9f2fa888cf0df24d995093f67b93aaf8478
soulnicee <soulnicee@gmail.com>
2023-11-23 13:27:40 +0200

    router changes

--- homework-12/task-1/src/router/index.js
+++ homework-12/task-1/src/router/index.js
@@ -1,63 +1,5 @@
-// import { createRouter, createWebHistory } from "vue-router";
-// import HomeView from "@/views/HomeView.vue";
-// const routes = [
-//   {
-//     path: "/",
-//     name: "home",
-//     component: HomeView,
-//   },
-//   {
-//     path: "/workers",
-//     name: "workers",
-//     component: () => import("@/views/WorkersView.vue"),
-//   },
-//   {
-//     path: "/workers/edit/:id?",
-//     name: "workers-edit",
-//     component: () => import("@/views/WorkersEditView.vue"),
-//     props: (route) => ({workerId: parseInt(route.params.id)})
-//   },
-//   {
-//     path: "/aplicants",
-//     name: "aplicants",
-//     component: () => import("@/views/AplicantsView.vue"),
-//   },
-//   {
-//     path: "/aplicants/edit/:id?",
-//     name: "aplicants-edit",
-//     component: () => import("@/views/AplicantsEditView.vue"),
-//     props: (route) => ({aplicantId: parseInt(route.params.id)})
-//   },
-//   {
-//     path: "/interviews",
-//     name: "interviews",
-//     component: () => import("@/views/InterviewsView.vue"),
-//   },
-//   {
-//     path: "/interviews/edit/:id?",
-//     name: "interviews-edit",
-//     component: () => import("@/views/InterviewsEditView.vue"),
-//     props: (route) => ({interviewId: parseInt(route.params.id)})
-//   },
-// ];
-
-// const router = createRouter({
-//   history: createWebHistory(process.env.BASE_URL),
-//   routes,
-// });
-
-// export default router;
-
-
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
-import WorkersView from "@/views/WorkersView.vue";
-import WorkersEditView from "@/views/WorkersEditView.vue";
-import AplicantsView from "@/views/AplicantsView.vue";
-import AplicantsEditView from "@/views/AplicantsEditView.vue";
-import InterviewsView from "@/views/InterviewsView.vue";
-import InterviewsEditView from "@/views/InterviewsEditView.vue";
-
 const routes = [
   {
     path: "/",
@@ -67,34 +9,34 @@ const routes = [
   {
     path: "/workers",
     name: "workers",
-    component: WorkersView,
+    component: () => import("@/views/WorkersView.vue"),
   },
   {
     path: "/workers/edit/:id?",
     name: "workers-edit",
-    component: WorkersEditView,
+    component: () => import("@/views/WorkersEditView.vue"),
     props: (route) => ({workerId: parseInt(route.params.id)})
   },
   {
     path: "/aplicants",
     name: "aplicants",
-    component: AplicantsView,
+    component: () => import("@/views/AplicantsView.vue"),
   },
   {
     path: "/aplicants/edit/:id?",
     name: "aplicants-edit",
-    component: AplicantsEditView,
+    component: () => import("@/views/AplicantsEditView.vue"),
     props: (route) => ({aplicantId: parseInt(route.params.id)})
   },
   {
     path: "/interviews",
     name: "interviews",
-    component: InterviewsView,
+    component: () => import("@/views/InterviewsView.vue"),
   },
   {
     path: "/interviews/edit/:id?",
     name: "interviews-edit",
-    component: InterviewsEditView,
+    component: () => import("@/views/InterviewsEditView.vue"),
     props: (route) => ({interviewId: parseInt(route.params.id)})
   },
 ];
@@ -105,3 +47,61 @@ const router = createRouter({
 });
 
 export default router;
+
+
+// import { createRouter, createWebHistory } from "vue-router";
+// import HomeView from "@/views/HomeView.vue";
+// import WorkersView from "@/views/WorkersView.vue";
+// import WorkersEditView from "@/views/WorkersEditView.vue";
+// import AplicantsView from "@/views/AplicantsView.vue";
+// import AplicantsEditView from "@/views/AplicantsEditView.vue";
+// import InterviewsView from "@/views/InterviewsView.vue";
+// import InterviewsEditView from "@/views/InterviewsEditView.vue";
+
+// const routes = [
+//   {
+//     path: "/",
+//     name: "home",
+//     component: HomeView,
+//   },
+//   {
+//     path: "/workers",
+//     name: "workers",
+//     component: WorkersView,
+//   },
+//   {
+//     path: "/workers/edit/:id?",
+//     name: "workers-edit",
+//     component: WorkersEditView,
+//     props: (route) => ({workerId: parseInt(route.params.id)})
+//   },
+//   {
+//     path: "/aplicants",
+//     name: "aplicants",
+//     component: AplicantsView,
+//   },
+//   {
+//     path: "/aplicants/edit/:id?",
+//     name: "aplicants-edit",
+//     component: AplicantsEditView,
+//     props: (route) => ({aplicantId: parseInt(route.params.id)})
+//   },
+//   {
+//     path: "/interviews",
+//     name: "interviews",
+//     component: InterviewsView,
+//   },
+//   {
+//     path: "/interviews/edit/:id?",
+//     name: "interviews-edit",
+//     component: InterviewsEditView,
+//     props: (route) => ({interviewId: parseInt(route.params.id)})
+//   },
+// ];
+
+// const router = createRouter({
+//   history: createWebHistory(process.env.BASE_URL),
+//   routes,
+// });
+
+// export default router;
