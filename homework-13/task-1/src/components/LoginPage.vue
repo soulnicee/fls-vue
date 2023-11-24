bbdd88a91178dfb442ea0f69fd0f35c854cf9945
soulnicee <soulnicee@gmail.com>
2023-11-24 14:12:53 +0200

    added files

--- homework-13/task-1/src/components/LoginPage.vue
+++ homework-13/task-1/src/components/LoginPage.vue
@@ -1,14 +1,12 @@
 <template>
-  <div class="home">
+  <div>
   </div>
 </template>
 
 <script>
-
-
 export default {
-  name: "HomeView",
-  components: {
-  },
-};
+  name: 'LoginPage'
+}
 </script>
+
+<style lang="scss" scoped></style>
---
+++ homework-13/task-1/src/views/LoginView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <login-page />
+  </div>
+</template>
+
+<script>
+import LoginPage from "@/components/LoginPage.vue";
+export default {
+  name: 'LoginView',
+  components: {
+    LoginPage,
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
