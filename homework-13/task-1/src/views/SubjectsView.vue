2cc11abb33cba29d8022f751fa05868c836cfc2f
soulnicee <soulnicee@gmail.com>
2023-11-25 11:42:10 +0200

    some changes

--- homework-13/task-1/src/views/SubjectsView.vue
+++ homework-13/task-1/src/views/SubjectsView.vue
@@ -1,17 +1,71 @@
 <template>
-  <div class="subjects-list">
-    <subjects-list />
+  <div class="subjects">
+    <h1 class="subjects__title">Вибір уроків</h1>
+    <ol v-if="subjectsList.length" class="subjects__list">
+      <li v-for="subject in subjectsList" :key="subject.id" class="subject-checkbox">
+        <label :for="subject.id" class="subject-checkbox__label">{{ subject.name }}</label>
+        <input :id="subject.id" v-model="selectedSubjects" class="subject-checkbox__input" type="checkbox" :value="subject.id">
+      </li>
+    </ol>
+    <div v-else class="empty-list">Сталася помилка. Зачекайте трошки!</div>
+    <button type="button" class="subjects__btn">Підтвердити вибір</button>
   </div>
 </template>
 
 <script>
-import SubjectsList from "@/components/SubjectsList.vue";
+import { mapGetters } from 'vuex';
 export default {
   name: 'SubjectsView',
-  components: {
-    SubjectsList,
+  data() {
+    return {
+      selectedSubjects: []
+    }
+  },
+  computed: {
+    ...mapGetters('subjects', ['subjectsList'])
   },
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.subjects {
+  margin: 0 auto;
+  text-align: center;
+  padding: 10px 10px 50px 10px;
+
+  // .subjects__title
+  &__title {
+    font-size: 26px;
+
+    &:not(:last-child) {
+      margin-bottom: 10px;
+    }
+  }
+
+  // .subjects__list
+  &__list {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    &:not(:last-child) {
+      margin-bottom: 20px;
+    }
+  }
+
+  &__btn {
+    font-size: 18px;
+    padding: 5px 20px;
+    border-radius: 5px;
+    cursor: pointer;
+    background-color: #d6d190;
+    transition: all 0.3s;
+
+    @media (any-hover: hover) {
+      &:hover {
+        background-color: #98e983;
+      }
+    }
+  }
+}
+</style>
