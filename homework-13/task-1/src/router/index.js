49afdb69ccaf685c2007511f64339e21b0640bec
soulnicee <soulnicee@gmail.com>
2023-11-27 19:56:41 +0200

    some changes

--- homework-13/task-1/src/router/index.js
+++ homework-13/task-1/src/router/index.js
@@ -1,52 +1,59 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-
+import LoginView from "../views/LoginView.vue";
+import PageNotFoundView from "../views/PageNotFoundView.vue";
+import store from "@/store/index.js";
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+    meta: {auth: false}
   },
   {
     path: "/login",
     name: "login",
-    component: () => import(/* webpackChunkName: "login" */ "../views/LoginView.vue"),
+    component: LoginView,
+    meta: {auth: false}
   },
   {
     path: "/lessons/select",
     name: "select_lessons",
     component: () => import(/* webpackChunkName: "lessons" */ "../views/SubjectsView.vue"),
+    meta: {auth: true}
   },
   {
     path: "/lessons/:lessonsId+",
     name: "select_teachers",
     props: (route) => ({selectedSubjectsList: route.params.lessonsId.map((item) => parseInt(item))}),
     component: () => import(/* webpackChunkName: "lessons" */ "../views/TeachersView.vue"),
+    meta: {auth: true}
   },
   {
     path: "/lessons/:lessonPair+/",
     name: "lessons_list",
-    // props: (route) => {
-    //   const lessonPair = route.params.lessonPair
-    //   const lessonId = lessonPair.map(pair => parseInt(pair.split('-')[0]));
-    //   const teacherId = lessonPair.map(pair => parseInt(pair.split('-')[1]));
-    //   return {
-    //     selectedSubjectId: lessonId,
-    //     selectedTeacherId: teacherId,
-    //   }
-    // },
     component: () => import(/* webpackChunkName: "lessons" */ "../views/LessonsView.vue"),
+    meta: {auth: true}
   },
   {
-    path: "/404",
-    name: "404",
-    component: () => import(/* webpackChunkName: "404" */ "../views/PageNotFoundView.vue"),
-  },
+		path: '/:pathMatch(.*)*',
+		name: '404',
+		component: PageNotFoundView,
+		meta: { auth: false }
+	},
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
 });
-
+router.beforeEach((to) => {
+	const isAuth = store.getters.isAuth;
+	if (to.meta.auth && isAuth) {
+		return {
+			name: 'login',
+			query: { redirect: to.fullPath },
+		}
+	}
+})
 export default router;
--- homework-13/task-1/src/views/PageNotFoundView.vue
+++ homework-13/task-1/src/views/PageNotFoundView.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
-  </div>
+  <div> сторінка не знайдена </div>
 </template>
 
 <script>
+
 export default {
   name: 'PageNotFoundView'
 }
