3a11ca769b72e8674b7eafde6cbdb7e533ec71bf
soulnicee <soulnicee@gmail.com>
2023-11-30 13:32:35 +0200

    changes

---
+++ homeworl-14/task-1/src/data/productsList.js
@@ -0,0 +1,26 @@
+export const productsList = [
+  {
+    id:1,
+    imageSrc: 'https://content.rozetka.com.ua/goods/images/big/295701523.jpg',
+    title: 'Макароны Цельнозерновые Gnocchi TORINO',
+    price:'150',
+  },
+  {
+    id:2,
+    imageSrc: 'https://content.rozetka.com.ua/goods/images/big/295701523.jpg',
+    title: 'Колбаса сыровяленая Discovery Суджук',
+    price:'300',
+  },
+  {
+    id:3,
+    imageSrc: 'https://content.rozetka.com.ua/goods/images/big/289064226.jpg',
+    title: 'Булочка для бургера з кунжутом',
+    price:'20',
+  },
+  {
+    id:4,
+    imageSrc: 'https://content2.rozetka.com.ua/goods/images/big/163285436.jpg',
+    title: 'Сыр Grana Padano',
+    price:'800',
+  }
+]
